2904dda6dc9d0d3837526aad0d52f575f2b3d277
linzexin <linzexinmasterchief@gmail.com>
2020-04-21 23:21:00 +0800

    refine turret auto aim control

--- Assets/Scripts/turret/Turret_aim.cs
+++ Assets/Scripts/turret/Turret_aim.cs
@@ -28,16 +28,47 @@ public class Turret_aim : MonoBehaviour
         }
         // stop playing idle animation and transit to shoot ready stage
         gameObject.GetComponent<Animator>().SetBool("target_found", true);
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+
+        Vector3 target_direction_to_gun = other.transform.position - Turret_gun_base.transform.position;
+        Vector3 target_direction_to_turret_top = other.transform.position - Turret_top.transform.position;
+
+        float horizontal_angle = RadToDeg(
+            Mathf.Atan2(target_direction_to_turret_top.x,
+            target_direction_to_turret_top.z)
+            ) + 180;
+
+        // cos(theta) = a*b / (|a|*|b|)
+        float vertical_angle = 90 - RadToDeg(
+            Mathf.Acos(Vector3.Dot(transform.up, target_direction_to_gun) / target_direction_to_gun.magnitude)
+            );
+
+        float horizontal_rotation = horizontal_angle - Turret_top.transform.localRotation.y;
+
+        //float horizontal_delta_rotation = 0;
+        //// check if target angle is at the limit of going to far
+        //if (Mathf.Abs(horizontal_rotation) > 2)
+        //{
+        //    horizontal_delta_rotation = horizontal_rotation % 1f;
+
+        //    
+        //}
+        //else
+        //{
+        //}
+        Turret_top.transform.localRotation = Quaternion.Euler(0, 0, horizontal_rotation);
+            
+
+        Turret_gun_base.transform.localRotation = Quaternion.Euler(Mathf.LerpAngle(Turret_gun_base.transform.rotation.x, vertical_angle, Time.time / 4), 0, 0);
+
+        if (Input.GetKey(KeyCode.Space))
         {
             gameObject.GetComponent<Animator>().SetBool("shoot", true);
         }
-
-        float horizontal_angle = Mathf.Atan2(other.transform.position.x - transform.position.x, other.transform.position.z - transform.position.z);
-
-        Debug.Log(Mathf.LerpAngle(Turret_top.transform.rotation.y, (horizontal_angle) + 180, 1f));
-        Turret_top.transform.Rotate(new Vector3(0, 1, 0), Mathf.LerpAngle(Turret_top.transform.rotation.y, (horizontal_angle) + 180, 1f));
-
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("shoot", false);
+        }
     }
 
     private void OnTriggerExit(Collider other)
