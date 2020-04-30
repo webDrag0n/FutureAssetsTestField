6e778ceb78d645df75cc6f3417a9c593f2bed10a
linzexin <linzexinmasterchief@gmail.com>
2020-04-30 13:26:11 +0800

    Added Ragdoll test scene

---
+++ Assets/Scripts/Ragdoll/GiveInitialForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveInitialForce : MonoBehaviour
+{
+    public Vector3 Force_vector;
+    public bool is_impulse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (is_impulse)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Force_vector, ForceMode.Impulse);
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Force_vector);
+        }
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Time.timeScale = 0.5f;
+    }
+}
