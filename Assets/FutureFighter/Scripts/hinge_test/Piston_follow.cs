c829ff5e3f84190e3cf76ae316321664aa101e94
linzexin <linzexinmasterchief@gmail.com>
2020-06-12 20:10:08 +0800

    trying to build physical landing gear

---
+++ Assets/FutureFighter/Scripts/hinge_test/Piston_follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston_follow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 piston_distance;
+    private Vector3 delta_pos;
+    private Quaternion delta_rot;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        piston_distance = Vector3.zero;
+        delta_pos = target.position - transform.position;
+        delta_rot = target.rotation * transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = target.position - delta_pos - piston_distance;
+        transform.LookAt(target, Vector3.up);
+    }
+}
---
+++ Assets/FutureFighter/Scripts/hinge_test/Piston_limit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston_limit : MonoBehaviour
+{
+
+    private Vector3 init_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, init_pos.y, 0);
+    }
+}
